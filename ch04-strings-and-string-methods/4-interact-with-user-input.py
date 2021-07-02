84f00d40df76d25a67bef5c701a2b800c9490a9e
selviig07 <66674708+selviig07@users.noreply.github.com>
2021-07-02 23:07:58 +0530

    Update 4-interact-with-user-input.py

--- ch04-strings-and-string-methods/4-interact-with-user-input.py
+++ ch04-strings-and-string-methods/4-interact-with-user-input.py
@@ -4,7 +4,7 @@
 
 # Exercise 1
 # Take input from the user and display that input back
-my_input = input("Type something: ")
+my_input = input("Type something:")
 print(my_input)
 
 
@@ -15,5 +15,5 @@ print(my_input.lower())
 
 # Exercise 3
 # Take user input and display the number of input characters.
-input_string = input("Type something else: ")
+input_string = input("Type something else:")
 print(len(input_string))
