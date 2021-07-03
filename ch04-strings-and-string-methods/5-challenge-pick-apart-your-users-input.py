3224445d7ba093736f702d1f4e101d42bf2c44e5
selviig07 <66674708+selviig07@users.noreply.github.com>
2021-07-03 23:00:36 +0530

    Update 5-challenge-pick-apart-your-users-input.py

--- ch04-strings-and-string-methods/5-challenge-pick-apart-your-users-input.py
+++ ch04-strings-and-string-methods/5-challenge-pick-apart-your-users-input.py
@@ -4,6 +4,6 @@
 
 # Return the upper-case first letter entered by the user
 
-user_input = input("Tell me your password: ")
-first_letter = user_input[0]
-print("The first letter you entered was: " + first_letter.upper())
+user_input = input("Tell me your password:")
+first_letter =user_input[0]
+print("The first letter you entered was:" + first_letter.upper())
